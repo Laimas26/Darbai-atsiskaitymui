594da36e5882b3c17de8df498ef0efc5031234b2
laimas26 <laimonas.rupeika@gmail.com>
2023-12-14 18:36:22 +0200

    Padaryta 3 uzduotis

---
+++ 3 uzduotis-beatiful-string.cpp
@@ -0,0 +1,53 @@
+/******************************************************************************
+
+                             Pirmasis darbas
+
+Uzduotis: https://codeforces.com/problemset/problem/1265/A
+                             
+*******************************************************************************/
+using namespace std;
+
+#include <set>;
+#include <iostream>
+
+int boyOrGirl(set<char> disChar) {
+    // 
+    int res;
+    if (disChar.size() % 2 == 0)
+        res = 1;
+    else
+        res = 0;
+
+    return res;
+}
+
+set<char> distinctCharacters(string name)
+{
+    set<char> distinctChars(name.begin(), name.end());
+
+    return distinctChars;
+}
+
+void printResult(int result)
+{
+    if (result == 1)
+        cout << "CHAT WITH HER!" << endl;
+    else
+        cout << "IGNORE HIM!" << endl;
+}
+
+int main() {
+    // Nuskaitoma zmogaus ivestis
+    string name;
+
+    cout<<"Iveskite varda: ";
+    cin >> name;
+    
+    // Grazina rezultata, 0 = Male, 1 = Female
+    int result = boyOrGirl(distinctCharacters(name));
+
+    printResult(result);
+
+
+    return 0;
+}
